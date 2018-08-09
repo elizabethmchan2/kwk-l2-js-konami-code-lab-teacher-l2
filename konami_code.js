53f62701256d9c11f6459330e00adbf9e3e3e08e
Elizabeth Chan <lmchan@brynmawr.edu>
2018-08-09 14:42:39 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -16,18 +16,18 @@ const codes = [
 
 function init() {
   document.addEventListener('keydown', function(e) {
-   const key = e.key;
-   
-    if (key === alphabet[index]) {
-      index++;
-   
-      if (index === alphabet.length) {
-        alert("Hurray!");
-   
-        index = 0;
-      }
-    } else {
-      index = 0;
-    }
+  const key = e.key;
+  
+  if (key === alphabet[index]) {
+  index++;
+  
+  if (index === alphabet.length) {
+  alert("Hurray!");
+  
+  index = 0;
+  }
+  } else {
+  index = 0;
+  }
   })
 }
