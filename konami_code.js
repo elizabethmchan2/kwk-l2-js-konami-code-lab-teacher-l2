eea30d76555b814f33c4744613335b40257078b1
Elizabeth Chan <lmchan@brynmawr.edu>
2018-08-09 10:50:40 -0400

    Update konami_code.js

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,24 @@ const codes = [
   "a"
 ];
 
+
+
+
+
 function init() {
-  // your code here
+  var index = 0;
+  document.addEventListener('keydown', function(e) {
+    const key = e.key;
+    
+    if (key === codes[index]) {
+      console.log(key + index)
+      index++;
+      if (index === codes.length) {
+        alert("Hurray!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  })
 }
